51be4e97872a3ad8b868edf907a238b3f4685e69
Celeste Roselli <celesteroselli27@gmail.com>
2023-09-16 15:56:50 -0400

    Scanner locked out for 5 minutes after scanning

--- GlobalHackathonApp/ScannerView.swift
+++ GlobalHackathonApp/ScannerView.swift
@@ -11,23 +11,40 @@ import CodeScanner
 
 struct ScannerView: View {
     @Binding var points: Int
+    @AppStorage("date") var date: Int = Int(UInt64((Date().timeIntervalSince1970)))
+    
+    func check_time() -> Bool {
+        let now: Int = Int(UInt64((Date().timeIntervalSince1970)))
+        if now < (date + 300) {
+            return false
+        } else {
+            return true
+        }
+    }
     
     var body: some View {
         NavigationView {
             VStack {
-                CodeScannerView(codeTypes: [.qr], simulatedData: "Success") { response in
-                    switch response {
-                    case .success(let result):
-                        if (result.string) == "Success" {
-                            print("Points added!")
-                            points += 1
-                            //trigger a modal to pop out saying that points were added
-                            
+                if check_time() {
+                    CodeScannerView(codeTypes: [.qr], simulatedData: "Success") { response in
+                        switch response {
+                        case .success(let result):
+                            if (result.string) == "Success" {
+                                print("Points added!")
+                                date = Int(UInt64((Date().timeIntervalSince1970)))
+                                points += 1
+                                //trigger a modal to pop out saying that points were added
+                                
+                            }
+                        case .failure(let error):
+                            //handles error
+                            print(error.localizedDescription)
                         }
-                    case .failure(let error):
-                        //handles error
-                        print(error.localizedDescription)
                     }
+                } else {
+                    Text("Sorry! To prevent fraud, the scanner is locked out for 5 minutes after scanning a code. Check back in 5 minutes!")
+                        .font(.title)
+                        .multilineTextAlignment(.center)
                 }
             }
         }.navigationTitle("QR Scanner")
