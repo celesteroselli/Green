acc678dc734abf8850539191834cc7d50397eb65
odawg <odawg@duck.com>
2023-09-15 21:41:12 -0400

    Was able to add back Sign in with Uber and integrate it with onboarding, but onLime breaks onboarding and is commented out.

--- GlobalHackathonApp/OnboardingAndHome.swift
+++ GlobalHackathonApp/OnboardingAndHome.swift
@@ -15,25 +15,27 @@ struct FirstPage: View {
 
 struct OnboardingView: View {
     
-     var uber: Uber = Uber()
-     var lime: Lime = Lime()
-     @State public var needsOnboarding: Bool = true
-     @State var uberLoginMessage: String = ""
-     @State var uberLoginMessageColor: Color = Color.gray
-     @State var limePhoneNumberInputCurrent: String = ""
-     
-     func onUberLoginAttempt(success: Bool, message: String) {
-     if success {
-     self.uberLoginMessage = "Success"
-     self.uberLoginMessageColor = Color.green
-     } else {
-     self.uberLoginMessage = "Error: " + message
-     self.uberLoginMessageColor = Color.red
-     }
-     }
-     
+    var uber: Uber = Uber()
+    var lime: Lime = Lime()
+    @State public var needsOnboarding: Bool = true
+    @State var uberLoginMessage: String = ""
+    @State var uberLoginMessageColor: Color = Color.gray
+    @State var limePhoneNumberInputCurrent: String = ""
+    
+    func onUberLoginAttempt(success: Bool, message: String) {
+        if success {
+            self.uberLoginMessage = "Success"
+            self.uberLoginMessageColor = Color.green
+        } else {
+            self.uberLoginMessage = "Error: " + message
+            self.uberLoginMessageColor = Color.red
+        }
+    }
+    
     var body: some View {
+        
         NavigationView {
+            
             TabView {
                 
                 
@@ -47,7 +49,9 @@ struct OnboardingView: View {
                     .padding(.all)
                     .frame(width: 6.0, height: 7.0)
                     .imageScale(.large)
-                
+                //         Button("linkGreen+Uber") {
+                //nothingyet
+                //             }
                 
                 
                 
@@ -61,47 +65,84 @@ struct OnboardingView: View {
                 
                 
                 Text("Let's Get Started!")
-                    .font(.largeTitle)
-                
-                var READMETOO = "This button is throwing off the onboarding because of a bug. (idk why)"
-               // Button("Link Uber to Green \(Image(systemName: "link.badge.plus"))") {
-                    //thisbuttonisdead
-                    //          uber.doLogin(onCompletion: onUberLoginAttempt)
+                    .font(.largeTitle.bold())
+                /*
+                 TextField("Enter Your Lime Phone Number", text: $limePhoneNumberInputCurrent) {
+                 .keyboardType(.phonePad)
+                 .onChange(of: limePhoneNumberInputCurrent) { newText in
+                 if (limePhoneNumberInputCurrent.count == 3) {
+                 limePhoneNumberInputCurrent = limePhoneNumberInputCurrent + "-"
+                 } else if (limePhoneNumberInputCurrent.count == 8) {
+                 limePhoneNumberInputCurrent = limePhoneNumberInputCurrent + "-"
+                 }
+                 }
+                 .onSubmit {
+                 lime.sendLimeConfCode(phoneInput: limePhoneNumberInputCurrent) { success in
+                 if success {
+                 print("success")
+                 }
+                 }
+                 }
+                 */
+                Button("Link Uber to Green", systemImage: "person.crop.circle.fill.badge.plus") {
                     
+                    uber.doLogin(onCompletion: onUberLoginAttempt)
                     
                 }
-                .tabViewStyle(.page(indexDisplayMode: .never))
+                .buttonStyle(.borderedProminent)
+                var READMETOO = "This button is throwing off the onboarding because of a bug. (idk why)"
+                //  Button("Link Uber to Green \(Image(systemName: "link.badge.plus"))") {
+                //thisbuttonisdead
+                //  uber.doLogin(onCompletion: onUberLoginAttempt)
+                //  Button("linkGreen+Uber") {
+                //nothingyet
+                //      }
+                // Button("Link Uber to Green \(Image(systemName: "link.badge.plus"))") {
+                //thisbuttonisdead
+                //   uber.doLogin(onCompletion: onUberLoginAttempt)
+                //       }
+                
+                /*
+                 TextField("Lime Phone Number", text: $limePhoneNumberInputCurrent) {
+                 //          .keyboardType(.phonePad)
+                 .onChange(of: limePhoneNumberInputCurrent) { newText in
+                 if (limePhoneNumberInputCurrent.count == 3) {
+                 limePhoneNumberInputCurrent = limePhoneNumberInputCurrent + "-"
+                 } else if (limePhoneNumberInputCurrent.count == 8) {
+                 limePhoneNumberInputCurrent = limePhoneNumberInputCurrent + "-"
+                 }
+                 }
+                 .onSubmit {
+                 lime.sendLimeConfCode(phoneInput: limePhoneNumberInputCurrent) { success in
+                 if success {
+                 print("success")
+                 }
+                 }
+                 
+                 
+                 }
+                 
+                 }
+                 */
+                
+                //      Text(uberLoginMessage)
+                //       .background(uberLoginMessageColor)
+                
+                
+                var README = "CODE BELOW IS NOT INCLUDED IN ONBOARDING DUE TO XCODE LIMITATIONS"
+                
+                
+                
+                
+                
+                
             }
+            .tabViewStyle(.page(indexDisplayMode: .never))
         }
-        //      Text(uberLoginMessage)
-        //       .background(uberLoginMessageColor)
-    
-    
-    var README = "CODE BELOW IS NOT INCLUDED IN ONBOARDING DUE TO XCODE LIMITATIONS"
-    
-    
-        /*   TextField("Lime Phone Number", text: $limePhoneNumberInputCurrent)
-         .keyboardType(.phonePad)
-         .onChange(of: limePhoneNumberInputCurrent) { newText in
-         if (limePhoneNumberInputCurrent.count == 3) {
-         limePhoneNumberInputCurrent = limePhoneNumberInputCurrent + "-"
-         } else if (limePhoneNumberInputCurrent.count == 8) {
-         limePhoneNumberInputCurrent = limePhoneNumberInputCurrent + "-"
-         }
-         }
-         .onSubmit {
-         lime.sendLimeConfCode(phoneInput: limePhoneNumberInputCurrent) { success in
-         if success {
-         print("success")
-         }
-         }
-         }
-         */
         
+        //#Preview {
+        //    LanchScreen()
+        //}
         
     }
-    
-
-//#Preview {
-//    LanchScreen()
-//}
+}
